13884d9eb258492f937b0ce9ad8e751e07450d28
zsaraf <zsaraf@stanford.edu>
2013-03-22 06:17:32 -0400

    Fully functional save and load, ideally?

--- repostats.py
+++ repostats.py
@@ -4,12 +4,14 @@ import sys
 import copy
 import subprocess
 
+real_path = os.path.realpath(__file__)
+os.chdir(real_path[:real_path.rfind('/')])
+
 sys.path.insert(0, os.path.abspath(os.getcwd() + '/.helpers'))
 
 import load as load
 import save as save
 import globaldata as globaldata
-
 
 keywords = ['load=','ls','file=', 'save', 'lang', 'clear']
 import getopt
@@ -17,19 +19,6 @@ opts, remainder = getopt.getopt(sys.argv[1:],'p:l',keywords)
 for o,p in opts:
     if o in ('-n','--load'):
         load.loadRepo(p)
-        
-        index = p.rfind('/')
-        if (index == p.__len__() - 1):
-            p = p.rstrip('/')
-            index = p.rfind('/')
-        repo_name = p[index + 1:]
-        print repo_name
-        print os.getcwd()
-        os.chdir('.data')
-        process = subprocess.Popen('mv ' + repo_name + '/ repo/', shell=True)
-        process.wait()
-        os.chdir('../')
-        sys.exit()
     elif o in ('-l','--ls'):
         x = 0
         #list files
