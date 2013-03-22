8f3b4d0d3001e12f80fd75bb5f0538d56ec34c72
calvinstudebaker <calstud@stanford.edu>
2013-03-22 10:58:14 -0700

    ignores .git during ls

--- .helpers/showfiles.py
+++ .helpers/showfiles.py
@@ -9,14 +9,15 @@ def getDepth(dirpath):
 
 
 def displayFiles():
-    if (not datastorage.repoIsStored()):
-        print 'No repository is currently loaded.  Please load one and try again.'  
-        print 'Type ./repostats --help for more information'
-        sys.exit()
-        space = "   "
-    for dirpath, dirs, files in os.walk(os.getcwd() + '/.data/repo'):
-	    current = dirpath[dirpath.rfind('/'):len(dirpath)]
-	    depth = getDepth(dirpath)
-	    print depth*space + current
-	    for filename in files:
-		    print space*(depth+1) + filename
+	if (not datastorage.repoIsStored()):
+		print 'No repository is currently loaded.  Please load one and try again.'  
+		print 'Type ./repostats --help for more information'
+		sys.exit()
+	space = "   "
+	for dirpath, dirs, files in os.walk(os.getcwd() + '/.data/repo'):
+		if '.git' in dirpath: continue
+		current = dirpath[dirpath.rfind('/'):len(dirpath)]
+		depth = getDepth(dirpath)
+		print depth*space + current
+		for filename in files:
+			print space*(depth+1) + filename
