3301274e53bb274466c8079fa5fe4ddcdb482601
Paul Julius Martinez <Paul@dn51uk0c.sunet>
2013-03-22 02:52:43 -0700

    globaldata is fully functional

--- .helpers/save.py
+++ .helpers/save.py
@@ -3,7 +3,12 @@ import sys
 import copy
 import subprocess
 
+import globaldata as globaldata
+
 def saveRepo():
 	path = os.getcwd()
-	print 'mv ' + path + '/.data/repo ' + path + '/'
-	process = subprocess.Popen('mv "' + path + '/.data/repo" "' + path + '/"', shell=True).wait()
+	if globaldata.isRepoStored() is False:
+		print 'shit' # Throw error!
+	else:
+		repoName = globaldata.getRepoName()
+		process = subprocess.Popen('mv "' + path + '/.data/repo" "' + path + '/' + repoName + '"', shell=True).wait()
