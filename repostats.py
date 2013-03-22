dc71ba4eb738a25a32f507b22bd6c1a33f892315
zsaraf <zsaraf@stanford.edu>
2013-03-22 03:50:08 -0400

    Checking environment things

--- repostats.py
+++ repostats.py
@@ -4,24 +4,42 @@ import sys
 import copy
 import subprocess
 
-file = ''
-encoded_file = '/home/zach/Code/CMessAround/ImportantInfo'
 
-keywords = ['password=','ls']
+file = os.getcwd()
+print 'CWD: ' + file
+for dirname,dirnames, filenames in os.walk('.'):
+    print dirname
+process = subprocess.Popen('ls -alrth .', shell=True)
+process.wait()
+sys.exit()
+
+
+
+keywords = ['load=','ls','file=', 'save', 'lang', 'clear']
 import getopt
 opts, remainder = getopt.getopt(sys.argv[1:],'p:l',keywords)
 for o,p in opts:
     if o in ('-n','--load'):
-        #load new file
+        process = subprocess.Popen('mkdir ~/Code/CS184/gitanalytic/repostats/repository/', shell=True)
+        process.wait()
+        process = subprocess.Popen('git clone https://github.com/zsaraf/Viral/', shell=True)
+        process.wait()
+        process = subprocess.Popen('mv ./Viral ./repository')
+        process.wait()
     elif o in ('-l','--ls'):
+        x = 0
         #list files
     elif o in ('-f', '--file'):
+        x = 0
         #do file analytics
     elif o in ('-s', '--save'):
+        x = 0
         #save repository
     elif o is '--lang':
+        x = 0
         #print langs
     elif o is '--clear':
+        x = 0
         #clear
         
 f = open(encoded_file, 'r')
