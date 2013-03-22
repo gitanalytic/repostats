806f6aa00b6ce14f4fb778b29095256bfd9cbc3c
zsaraf <zsaraf@stanford.edu>
2013-03-22 07:57:12 -0400

    Line count now functional

--- .helpers/linecount.py
+++ .helpers/linecount.py
@@ -5,10 +5,22 @@ import subprocess
 import mimetypes
 
 def countLines():
-    print os.getcwd()
     os.chdir('.data/repo')
-    print os.getcwd()
-    process = subprocess.Popen('ls', shell=True)
 
+    total = 0
+
+    for (path, dirs, files) in os.walk(os.getcwd()):
+        if ('.git' in path):
+            continue
+        for filename in files:
+            mime = mimetypes.guess_type(path+filename, False)
+            if (mime[0] != None and mime[0].find('text') == 0):
+                with open(path + '/' + filename) as f:
+                    for i, l in enumerate(f):
+                        pass
+                total += i + 1
+                
+    print 'Number of lines in git repository: ' + str(total)
+    os.chdir('../../')
     sys.exit()
 
